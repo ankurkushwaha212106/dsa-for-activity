0cc1e657fd87eee61bf5d49a0574950360049fd4
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-06-24 21:47:19 +0530

    sim

--- triangle.cpp
+++ triangle.cpp
@@ -6,9 +6,9 @@ int main()
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)
+        for(int j=n;j<=1;j++)
         {
-            cout<<"*"<<"  ";
+            cout<< i <<"  ";
         }
         cout<<endl;
     }
