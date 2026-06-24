2abe6d4754966b0d3f999f89e635a7f8cabddad3
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-06-25 02:48:26 +0530

    decimal

---
+++ decimaltobinary.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    n = n / 2;
+    while(n > 0)
+    {
+     int ans = n % 2
+
+    }
+    
+}
